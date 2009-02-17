e2cfed187ffeb91df80a489ad511c9300b59eace
Ngoc Dao <ngocdaothanh@gmail.com>
2009-02-17 16:48:52 +0900

    Avoid error when DB is blank

--- modules/core/theme/init.rb
+++ modules/core/theme/init.rb
@@ -1,11 +1,13 @@
 # Themes, regions, and blocks are not reloaded during runtime
-CONF[:theme] = SiteConf.instance.theme
+CONF[:theme] = SiteConf.instance.theme rescue nil  # nil when DB is not initialized
 
-# Views in theme directory take top priority
-# Cannot use view_paths.unshift
-ActionController::Base.view_paths = %W(#{RAILS_ROOT}/themes/#{CONF[:theme]}/app/views) + ActionController::Base.view_paths
+unless CONF[:theme].nil?
+  # Views in theme directory take top priority
+  # Cannot use view_paths.unshift
+  ActionController::Base.view_paths = %W(#{RAILS_ROOT}/themes/#{CONF[:theme]}/app/views) + ActionController::Base.view_paths
 
-require "#{RAILS_ROOT}/themes/#{CONF[:theme]}/init"
+  require "#{RAILS_ROOT}/themes/#{CONF[:theme]}/init"
 
-regions_blocks = YAML.load(File.read("#{RAILS_ROOT}/themes/#{CONF[:theme]}/regions_blocks.yml"))
-CONF[:regions] = regions_blocks['regions'].map { |e| e.intern }
+  regions_blocks = YAML.load(File.read("#{RAILS_ROOT}/themes/#{CONF[:theme]}/regions_blocks.yml"))
+  CONF[:regions] = regions_blocks['regions'].map { |e| e.intern }
+end
