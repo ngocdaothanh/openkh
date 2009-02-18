97a0412af1ef903fd249f3dedea180b264d8c7be
Ngoc Dao <ngocdaothanh@gmail.com>
2009-02-18 13:39:38 +0900

    Emphasize current category

--- modules/core/category/app/helpers_hooks/categories_helper.rb
+++ modules/core/category/app/helpers_hooks/categories_helper.rb
@@ -3,7 +3,10 @@ module ApplicationHelper
     content = ''
     mod[:categories] << Category.uncategorized
     mod[:categories].each do |cat|
-      content << html_tree(cat) { |c, level| [link_to(c.name, category_path(:slug => c.slug))] }
+      content << html_tree(cat) do |c, level|
+        link = link_to(c.name, category_path(:slug => c.slug))
+        [(c == mod[:category])? content_tag(:em, link) : link ]
+      end
     end
     content = content_tag(:ul, content, :class => 'tree pages')
     [t('category.categories'), content]
