56ea2eb0091cc5eccca67c2809201c8bae057a8d
Ngoc Dao <ngocdaothanh@gmail.com>
2009-02-18 13:26:38 +0900

    Set default breadcrumb prefix to empty

--- modules/core/breadcrumb/app/models/breadcrumb_conf.rb
+++ modules/core/breadcrumb/app/models/breadcrumb_conf.rb
@@ -1,6 +1,6 @@
 class BreadcrumbConf < Conf
   acts_as_configurable do |c|
-    c.string  :prefix,            :default => I18n.t('breadcrumb_conf.default.prefix')
+    c.string  :prefix,            :default => ''
     c.string  :separator,         :default => '&raquo;'
     c.boolean :last_one_included, :default => true
   end
