a826317bd2a31bd129434965be1f1bcd67958f49
Ngoc Dao <ngocdaothanh@gmail.com>
2009-02-21 06:52:52 +0900

    Remove Compass (OpenKH will be designed to use existing Wordpress themes)

--- config/environment.rb
+++ config/environment.rb
@@ -16,10 +16,8 @@ require File.join(File.dirname(__FILE__), 'conf')
 #-------------------------------------------------------------------------------
 
 Rails::Initializer.run do |config|
-  # Compass needs HAML to be installed as gem, not in vendor/plugins/haml
   config.gem 'faker'
-  config.gem 'haml', :version => '>= 2.1'
-  config.gem 'chriseppstein-compass', :version => '>= 0.3.7', :lib => 'compass'
+  config.gem 'haml'
 
   # Evaluate gems.rb from all modules
   files = Dir.glob("#{RAILS_ROOT}/modules/**/gems.rb")
