36508f5f68e037a677260a203bc5bd80d870e5bb
Ngoc Dao <ngocdaothanh@gmail.com>
2009-03-28 08:52:39 +0900

    Add placeholders for "top users" and "top contents" block

--- modules/standard/statistics/app/helpers_hooks/helper.rb
+++ modules/standard/statistics/app/helpers_hooks/helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
-  def most_read_authors_block(lock)
+  def top_users_block(block)
+    users = User.all  # FIXME
+    [t('top_users_block.title'), render('statistics/top_users_block', :users => users)]
   end
 
-  def most_read_contents_block(block)
+  def top_contents_block(block)
+    contents = Article.all  # FIXME
+    [t('top_contents_block.title'), render('statistics/top_contents_block', :contents => contents)]
   end
 end
---
+++ modules/standard/statistics/app/models/top_contents_block.rb
@@ -0,0 +1,2 @@
+class TopContentsBlock < Block
+end
